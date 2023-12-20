8409eb49c2ebb0242c623298e0dea8c3e91d87c8
Abdul razak <abdulrazak.nasriudeen@gmail.com>
2023-12-20 20:02:49 +0530

    fibonacci series

---
+++ src/fibonacci/FibonacciSeries.java
@@ -0,0 +1,41 @@
+package fibonacci;
+
+import java.util.ArrayList;
+
+public class FibonacciSeries {
+    static ArrayList<Numbers> memory = new ArrayList<>();
+
+    public static long fibonacciSeries(int n){
+       long value =  Numbers.findKey(memory,n);
+       if(value != Integer.MIN_VALUE) return value;
+       else {
+       if(n <= 2 ) return 1 ;
+       long fib = fibonacciSeries(n-1) + fibonacciSeries(n-2);
+       memory.add(new Numbers(n,fib));
+       return fib;
+       }
+    }
+    public  static void main(String [] args){
+        System.out.println(fibonacciSeries(50));
+        System.out.println(memory.toString().toString());
+
+
+    }
+
+
+}
+class Numbers {
+    public  Numbers(int key , long value)
+    {
+        this.value = value;
+        this.key = key ;
+    }
+    int key ;
+    long value ;
+    public static  long findKey(ArrayList<Numbers> arrayList, int key){
+        for (Numbers numbers : arrayList){
+            if(key == numbers.key) return numbers.value;
+        }
+        return Integer.MIN_VALUE;
+    }
+}
