ab9534d57116eece4b10c688168fc586366ae19e
Abdul razak <abdulrazak.nasriudeen@gmail.com>
2023-12-21 14:00:31 +0530

    puzzle 2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("fibonacci of  50 is " + FibonacciSeries.fibonacciSeries(50));
+        System.out.println("The Sum of 1000 is " + NaturalNumbers.sumOfNaturalNumbers(1000));
+
+
     }
 }
